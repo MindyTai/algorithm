8ce0576b486b034064daf33121e066993523cae3
mindy <mindytai175@gmail.com>
2020-09-25 00:43:18 +0800

    leetcode 769(insertion sort related)

--- insertion_sort.js
+++ insertion_sort.js
@@ -14,3 +14,22 @@ const insertionSort = (arr) => {
 }
 
 insertionSort([2,1,9,76,1])
+
+
+//Leetcode: Max Chunks To Make Sorted
+var maxChunks = function(arr) {
+  let seperatedArrayCount = 0;
+  let maxValue = 0;
+
+  for(let i = 0; i < arr.length; i ++){
+      maxValue = Math.max(maxValue, arr[i])
+      if(i >= maxValue) {
+          ++ seperatedArrayCount
+      }
+  }
+  console.log(seperatedArrayCount);
+  return seperatedArrayCount
+};
+
+maxChunks([4,3,2,1,0]) //1
+maxChunks([1,0,2,3,4]) //4
